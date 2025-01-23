54f7c9bdd2cb66d1bd5502d065498a70ecf19f70
ferrofy <team.ferrofy@gmail.com>
2025-01-23 15:42:39 +0530

    Create Login_Page.js

---
+++ Login_Page.js
@@ -0,0 +1,12 @@
+const validUsernames = ['VPX', 'TANAV', 'AKX', 'KMX', 'SSX', 'ANGEL', 'UCI', 'PPX'];
+
+function validateUser() {
+    const usernameInput = document.getElementById('username').value.toUpperCase();
+    if (validUsernames.includes(usernameInput)) {
+        window.location.href = `https://ferrofy.github.io/Login/${usernameInput}`;
+    } else if (usernameInput === 'GUEST') {
+        window.location.href = `https://ferrofy.github.io/Login/${usernameInput}`;
+    } else {
+        document.getElementById('error-message').textContent = 'Guest, This User Name Is Incorrect. If You Are Guest Then Type User Name "Guest"';
+    }
+}
