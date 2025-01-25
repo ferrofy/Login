36c56956949e8a2b6fc15d0a4a862994e810cb8e
ferrofy <team.ferrofy@gmail.com>
2025-01-25 11:13:31 +0530

    Update Login_Page.js

--- JS/Login_Page.js
+++ JS/Login_Page.js
@@ -5,6 +5,6 @@ function validateUser() {
     if (validUsernames.includes(usernameInput) || usernameInput === 'GUEST') {
         window.location.href = `https://ferrofy.github.io/User/${encodeURIComponent(usernameInput)}/Profile`;
     } else {
-        document.getElementById('error-message').textContent = 'Guest, This User Name Is Incorrect. If You Are Guest Then Type User Name "Guest"';
+        document.getElementById('error-message').textContent = 'This User Name Is Not Valid. If You Are New Then Type User Name "Guest"';
     }
 }
