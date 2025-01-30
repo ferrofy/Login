6ad3b37870214253a445b29c0814e94aa3b34b35
ferrofy <team.ferrofy@gmail.com>
2025-01-30 15:55:38 +0530

    Update Login_Page.js

--- JS/Login_Page.js
+++ JS/Login_Page.js
@@ -3,7 +3,7 @@ const validUsernames = ['VPX', 'TANAV', 'AKX', 'KMX', 'SSX', 'ANGEL', 'UCI', 'PP
 function validateUser() {
     const usernameInput = document.getElementById('username').value.toUpperCase();
     if (validUsernames.includes(usernameInput) || usernameInput === 'NEW') {
-        window.location.href = `https://ferrofy.github.io/User/${encodeURIComponent(usernameInput)}/Profile`;
+        window.location.href = `https://ferrofy.github.io/Data/User/${encodeURIComponent(usernameInput)}/Profile`;
     } else {
         document.getElementById('error-message').textContent = 'This User Name Is Not Valid. If You Are New Then Type User Name "NEW"';
     }
